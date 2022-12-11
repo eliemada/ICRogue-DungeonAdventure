ff7b5e2ff60a03a4f19302ab6c3703d0d22f8701
sekuba <sebastian.kugler@epfl.ch>
2022-12-11 12:17:14 +0100

    javadoc for Connector.java

--- src/main/java/ch/epfl/cs107/play/game/icrogue/actor/Connector.java
+++ src/main/java/ch/epfl/cs107/play/game/icrogue/actor/Connector.java
@@ -14,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * A connector is an AreaEntity that allows the player to switch between rooms.
+ * It has the uncanny appearance of a door and can fittingly be opened, closed and locked.
+ */
 public class Connector extends AreaEntity {
 
     private State state;
@@ -29,54 +33,86 @@ public class Connector extends AreaEntity {
             (getOrientation().ordinal()+1)%2+1, getOrientation().ordinal()%2+1, this);
 
 
+    /**
+     * The possible states of a connector.
+     */
     public enum State {
         OPEN, CLOSED, LOCKED, INVISIBLE
     }
 
 
     /**
-     * Default AreaEntity constructor
+     * Default AreaEntity constructor augmented with the default state of a connector.
      *
      * @param area        (Area): Owner area. Not null
      * @param orientation (Orientation): Initial orientation of the entity in the Area. Not null
      * @param position    (DiscreteCoordinate): Initial position of the entity in the Area. Not null
      */
-
     public Connector(Area area, Orientation orientation, DiscreteCoordinates position) {
         super(area, orientation, position);
         state = State.INVISIBLE;
     }
 
+    /**
+     * Opens the connector if it was closed and unlocked.
+     */
     public void open() {
         if (state == State.CLOSED) state = State.OPEN;
     }
 
+    /**
+     * Tries to open the connector with a given keychain if it is locked,
+     * otherwise defers to open().
+     * @param keyIds (int[]): The keychain with which to try to unlock.
+     */
     public void open(ArrayList<Integer> keyIds) {
         if (((state == State.LOCKED) && (keyIds.size() > 0) && keyIds.contains(keyId))) state = State.OPEN;
         else open();
     }
 
+    /**
+     * Destination area setter, also closes the connector. (Default state is invisible)
+     * @param destArea (String): The name of the area to which the connector connects.
+     * @param destCoords (DiscreteCoordinates):
+     *                   The coordinates of arrival in the destination area.
+     */
     public void setDestination(String destArea, DiscreteCoordinates destCoords) {
         this.destArea = destArea;
         this.destCoords = destCoords;
         state = State.CLOSED;
     }
 
+    /**
+     * Destination area getter.
+     * @return (String): The name of the area to which the connector connects.
+     */
     public String getDestArea() {
         return destArea;
     }
 
+    /**
+     * Destination coordinates getter.
+     * @return (DiscreteCoordinates): The coordinates of arrival in the destination area.
+     */
     public DiscreteCoordinates getDestCoords() {
         return destCoords;
     }
 
+    /**
+     * Locks the connector with a given keyId if it is not invisible.
+     * (This prevents locking a connector that has no destination)
+     * @param keyId (int): The id of the key that (un)locks the connector.
+     */
     public void lockWithKey(int keyId) {
-        if (state == State.CLOSED) {
+        if (state != State.INVISIBLE) {
             state = State.LOCKED;
             this.keyId = keyId;
         }
     }
 
+    /**
+     * Closes a connector that was open.
+     */
     public void close() {
         if (state == State.OPEN) state = State.CLOSED;
     }
